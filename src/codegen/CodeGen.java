8275c9afd108e10681cfaf98ed512c4c91fb5b07
Mina <mina.ahmadiyazdi@gmail.com>
2021-01-22 11:47:37 +0330

    add SUB MUL DIV

--- src/codegen/CodeGen.java
+++ src/codegen/CodeGen.java
@@ -11,6 +11,7 @@ public class CodeGen {
     public static void PUSH(){
         semanticStack.push(token.getValue());
     }
+
     public static void ADD(){
         String src1 = semanticStack.pop();
         String src2 = semanticStack.pop();
@@ -21,6 +22,48 @@ public class CodeGen {
         String dName = "$" + pc;
         symboleTables.get(symboleTables.size() - 1).add(dName, d); //pc ast
         mipsCode.add(new Code("add",dName, src1, src2));
+        pc++;
+        semanticStack.push(dName);
+    }
+
+    public static void SUB(){
+        String src1 = semanticStack.pop();
+        String src2 = semanticStack.pop();
+        Dscp dscp1 = SymboleTable.find(src1);
+        Dscp dscp2 = SymboleTable.find(src2);
+        String type = getType(dscp1, dscp2, "Arith");
+        VariableDscp d = new VariableDscp(type);
+        String dName = "$" + pc;
+        symboleTables.get(symboleTables.size() - 1).add(dName, d); //pc ast
+        mipsCode.add(new Code("sub",dName, src1, src2));
+        pc++;
+        semanticStack.push(dName);
+    }
+
+    public static void MUL(){
+        String src1 = semanticStack.pop();
+        String src2 = semanticStack.pop();
+        Dscp dscp1 = SymboleTable.find(src1);
+        Dscp dscp2 = SymboleTable.find(src2);
+        String type = getType(dscp1, dscp2, "Arith");
+        VariableDscp d = new VariableDscp(type);
+        String dName = "$" + pc;
+        symboleTables.get(symboleTables.size() - 1).add(dName, d); //pc ast
+        mipsCode.add(new Code("mul",dName, src1, src2));
+        pc++;
+        semanticStack.push(dName);
+    }
+
+    public static void DIV(){
+        String src1 = semanticStack.pop();
+        String src2 = semanticStack.pop();
+        Dscp dscp1 = SymboleTable.find(src1);
+        Dscp dscp2 = SymboleTable.find(src2);
+        String type = getType(dscp1, dscp2, "Arith");
+        VariableDscp d = new VariableDscp(type);
+        String dName = "$" + pc;
+        symboleTables.get(symboleTables.size() - 1).add(dName, d); //pc ast
+        mipsCode.add(new Code("div",dName, src1, src2));
         pc++;
         semanticStack.push(dName);
     }
