e987cfa9ea9222fa7b9cb5220ad1944601d037d5
Fatemeh Mirabdollahi <fatemeh.1378.mir@gmail.com>
2021-01-28 20:43:04 +0330

    initial parser

---
+++ src/Compiler/Compiler.java
@@ -0,0 +1,79 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Stack;
+import codegen.Code;
+import codegen.CodeGen;
+import parser.ParseCell;
+import scanner.DecafScanner;
+import scanner.Token;
+import scanner.TokenType;
+
+public class Main {
+
+    public static DecafScanner scanner;
+    public static ArrayList<ArrayList<ParseCell>> parseTable;
+    public static ArrayList<String> parseTableHeader;
+    public static ArrayList<Code> mipsCode;
+    public static Stack<Integer> parseStack;
+    public static Stack<Token> semanticStack; //change
+    public static Token token;
+    public static int pc = 0;
+    public static int address = 0;
+    public static int temp = 0;
+    public static int labelNum = 0;
+    public static int maxTemp = 0;
+    public static int stringAddr = 0;
+    // t0 -> base of address
+    // t1 -> base of temp
+    // t2 -> base of double + 8
+    // t9 -> base of strings + 64
+    public static void main(String[] args) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        symboleTableInit();
+        scanner = new DecafScanner(new FileReader("src/chum.txt"));
+        parseStack = new Stack<>();
+        mipsCode = new ArrayList<>();
+        int curState = 0;
+        createParseTable();
+
+        while (true) {
+            token = scanner.tokenReader();
+            if (token.getType() == TokenType.undefined) {
+                //scanner error
+            }
+            ParseCell curCell = getCell(token, curState);
+            if (curCell == null) {
+                //parser error
+            } else {
+
+                //do action
+                doAction(curCell);
+
+                //invoke code gen
+                Method method = CodeGen.class.getMethod(curCell.getSemantic(), null);
+                method.setAccessible(true);
+                method.invoke(null, null);
+            }
+        }
+    }
+
+    private static void symboleTableInit() {
+    }
+
+    static ParseCell getCell(Token token, int state) {
+        return null;
+    }
+
+    static void createParseTable() {
+
+
+
+
+    }
+
+    static void doAction(ParseCell p) {
+
+    }
+}
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,80 +1,196 @@
-package parser;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Stack;
-import codegen.Code;
-import codegen.CodeGen;
-import scanner.DecafScanner;
-import scanner.Token;
-import scanner.TokenType;
+enum Action {
+    ERROR, SHIFT, GOTO, PUSH_GOTO, REDUCE, ACCEPT
+}
+
+class LLCell {
+    private Action action;
+    private int target;
+    private List<String> functions;
+
+    public LLCell(Action action, int target, List<String> functions) {
+        this.action = action;
+        this.target = target;
+        this.functions = functions;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public List<String> getFunction() {
+        return functions;
+    }
+}
+
 
 public class Parser {
-
-    public static DecafScanner scanner;
-    public static ArrayList<ArrayList<ParseCell>> parseTable;
-    public static ArrayList<String> parseTableHeader;
-    public static ArrayList<Code> mipsCode;
-    public static Stack<Integer> parseStack;
-    public static Stack<Token> semanticStack; //change
     public static Token token;
-    public static int pc = 0;
-    public static int address = 0;
-    public static int temp = 0;
-    public static int doubleAddr = 0;
-    public static int labelNum = 0;
-    public static int maxTemp = 0;
-    public static int stringAddr = 0;
-    // t0 -> base of address
-    // t1 -> base of temp
-    // t2 -> base of double + 8
-    // t9 -> base of strings + 64
-    public static ArrayList<String> notDefinedRecords ;
-    public static void main(String[] args) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        symboleTableInit();
-        scanner = new DecafScanner(new FileReader("src/chum.txt"));
-        parseStack = new Stack<>();
-        mipsCode = new ArrayList<>();
-        notDefinedRecords = new ArrayList<>();
-        int curState = 0;
-        createParseTable();
+    public static String TABLE_DELIMITER = ",";
+    private DecafScanner lexical;
+    private CodeGenerator codeGenerator;
+    private boolean debugMode;
 
-        while (true) {
-            token = scanner.tokenReader();
-            if (token.getType() == TokenType.undefined) {
-                //scanner error
+    private String[] symbols;
+    private LLCell[][] parseTable;
+    private int startNode;
+    private Deque<Integer> parseStack = new ArrayDeque<>();
+
+    private List<String> recoveryState;
+
+    public Parser(Lexical lexical, CodeGenerator codeGenerator, String nptPath, boolean debugMode) {
+        this(lexical, codeGenerator, nptPath);
+        this.debugMode = debugMode;
+    }
+
+    public Parser(Lexical lexical, CodeGenerator codeGenerator, String nptPath) {
+        this.lexical = lexical;
+        this.codeGenerator = codeGenerator;
+        this.recoveryState = new ArrayList<>();
+
+        if (!Files.exists(Paths.get(nptPath))) {
+            throw new RuntimeException("Parser table not found: " + nptPath);
+        }
+
+        try {
+            Scanner in = new Scanner(new FileInputStream(nptPath));
+            String[] tmpArr = in.nextLine().trim().split(" ");
+            int rowSize = Integer.parseInt(tmpArr[0]);
+            int colSize = Integer.parseInt(tmpArr[1]);
+            startNode = Integer.parseInt(in.nextLine());
+            symbols = in.nextLine().trim().split(TABLE_DELIMITER);
+
+            parseTable = new LLCell[rowSize][colSize];
+            for (int i = 0; i < rowSize; i++) {
+                tmpArr = in.nextLine().trim().split(TABLE_DELIMITER);
+                if (tmpArr.length != colSize) {
+                    throw new RuntimeException("Invalid .npt file: File contains rows with length" +
+                            " bigger than column size.");
+                }
+
+                for (int j = 0; j < colSize; j++) {
+                    String[] cellParts = tmpArr[j].split(" ");
+                    if (cellParts.length != 3) {
+                        throw new RuntimeException("Invalid .npt file: Parser cells must have extactly 3 values.");
+                    }
+                    Action action = Action.values()[Integer.parseInt(cellParts[0])];
+                    int target = Integer.parseInt(cellParts[1]);
+                    List<String> allFunctions;
+                    if (cellParts[2].equals("NoSem")) {
+                        allFunctions = new ArrayList<>();
+                    } else {
+                        allFunctions = Arrays.stream(cellParts[2].split("[;]"))
+                                .filter(s -> !s.isEmpty()).collect(Collectors.toList());
+                    }
+                    parseTable[i][j] = new LLCell(action, target, allFunctions);
+                }
             }
-            ParseCell curCell = getCell(token, curState);
-            if (curCell == null) {
-                //parser error
-            } else {
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Invalid .npt file.");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Unable to load .npt file.", e);
+        }
+    }
 
-                //do action
-                doAction(curCell);
-
-                //invoke code gen
-                Method method = CodeGen.class.getMethod(curCell.getSemantic(), null);
-                method.setAccessible(true);
-                method.invoke(null, null);
+    public void parse() {
+        int tokenID = nextTokenID();
+        int currentNode = startNode;
+        boolean accepted = false;
+        while (!accepted) {
+            String tokenText = symbols[tokenID];
+            LLCell cell = parseTable[currentNode][tokenID];
+            if (debugMode) {
+                System.out.println("Current token: text='" + symbols[tokenID] + "' id=" + tokenID);
+                System.out.println("Current node: " + currentNode);
+                System.out.println("Current cell of parser table: " +
+                        "target-node=" + cell.getTarget() +
+                        " action=" + cell.getAction() +
+                        " function=" + cell.getFunction());
+                System.out.println(String.join("", Collections.nCopies(50, "-")));
+            }
+            switch (cell.getAction()) {
+                case ERROR:
+                    updateRecoveryState(currentNode, tokenText);
+                    generateError("Unable to parse input.");
+                case SHIFT:
+                    doSemantics(cell.getFunction());
+                    tokenID = nextTokenID();
+                    currentNode = cell.getTarget();
+                    recoveryState.clear();
+                    break;
+                case GOTO:
+                    updateRecoveryState(currentNode, tokenText);
+                    doSemantics(cell.getFunction());
+                    currentNode = cell.getTarget();
+                    break;
+                case PUSH_GOTO:
+                    updateRecoveryState(currentNode, tokenText);
+                    parseStack.push(currentNode);
+                    currentNode = cell.getTarget();
+                    break;
+                case REDUCE:
+                    if (parseStack.size() == 0) {
+                        generateError("Unable to Reduce: token=" + tokenText + " node=" + currentNode);
+                    }
+                    updateRecoveryState(currentNode, tokenText);
+                    int graphToken = cell.getTarget();
+                    int preNode = parseStack.pop();
+                    doSemantics(parseTable[preNode][graphToken].getFunction());
+                    currentNode = parseTable[preNode][graphToken].getTarget();
+                    break;
+                case ACCEPT:
+                    accepted = true;
+                    break;
             }
         }
     }
 
-    private static void symboleTableInit() {
+    private void generateError(String message) {
+        System.out.flush();
+        System.out.println("Error happened while parsing ...");
+        for (String state : recoveryState) {
+            System.out.println(state);
+        }
+        throw new RuntimeException(message);
     }
 
-    static ParseCell getCell(Token token, int state) {
-        return null;
+    private void updateRecoveryState(int currentNode, String token) {
+        List<String> availableTokens = new ArrayList<>();
+        LLCell[] cellTokens = parseTable[currentNode];
+        for (int i = 0; i < cellTokens.length; i++) {
+            if (cellTokens[i].getAction() != Action.ERROR) {
+                availableTokens.add(symbols[i]);
+            }
+        }
+        recoveryState.add("At node " + currentNode + ": current token is " + token + " but except: " + availableTokens);
     }
 
-    static void createParseTable() {
-//todo
+    private int nextTokenID() {
+        token = lexical.tokenReader();
+        String tokenStr = token.getValue();
+        for (int i = 0; i < symbols.length; i++) {
+            if (symbols[i].equals(tokenStr)) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Undefined token: " + tokenStr);
     }
 
-    static void doAction(ParseCell p) {
-
+    private void doSemantics(List<String> functions) {
+        if (debugMode) {
+            System.out.println("Execute semantic codes: " + functions);
+        }
+        for (String function : functions) {
+            codeGenerator.doSemantic(function);
+        }
     }
 }
